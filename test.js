9b78482f2bbd4daf03b47b1fc119a452b3d0b248
sruthisurnedi <sruthisurnedi@gmail.com>
2021-06-23 18:14:23 +0530

    adding new things to child

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 console.log("welcome")
 console.log("How are you")
 
-// function sayHi(){
-//     return "hi"
-// }
+function sayHi(){
+    return "hi"
+}
 
-// let result1=sayHi()
+ let result1=sayHi()
 // let result2=sayHello()
 // console.log(result1)
 // console.log(result2)
